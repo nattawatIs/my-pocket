b1b4b00d7ef51368cfd6cd30503b0ea52d34bc69
Nattawat Isanpayu <nattawat@nattawat.local>
2024-11-22 22:47:16 +0700

    audit fix

--- frontend/App.js
+++ frontend/App.js
@@ -1,15 +1,12 @@
 import React from 'react'
-import { Text, View } from 'react-native';
-import { Provider } from 'react-redux'
 import AppNavigation from './navigation/appNavigation';
-import { store } from './redux/store';
 
 function App() {
 
   return (
-    <Provider store={store}>
+    <>
       <AppNavigation/>
-    </Provider>
+    </>
   );
 }
 
